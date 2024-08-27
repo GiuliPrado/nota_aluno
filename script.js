f0a464698a2767cab1ca367fb5d7ebf1f38906c5
Giuliana Prado <110168602+GiuliPrado@users.noreply.github.com>
2024-08-27 09:32:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Passo 1: Criar as constantes que irao armazenar as notas do aluno 
+
+const nota_1 = 10
+const nota_2 = 5
+const nota_3 = 4
+
+// Passo 2: Calcular a media das notas
+
+const soma_notas = nota_1 + nota_2 + nota_3
+const media = soma_notas / 3
+
+// Passo 3: Verificar se a nota e maior que 6
+
+if (media >= 6) { 
+    console.log('O aluno fi aprovado')
+}
+se media >= 4 && media < 6, console.log('esta de prova final') 
+else{
+    console.log('O aluno foi reprovado')
+}
+ calcula_nota_alunos(10,5,3)
